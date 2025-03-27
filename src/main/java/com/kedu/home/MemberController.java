370ed456bb97bb1527e9b1064a248c74b750f831
Xavier Choi <chaeins25@naver.com>
2025-03-27 16:05:44 +0900

    member basic CRUD fin

--- src/main/java/com/kedu/home/MemberController.java
+++ src/main/java/com/kedu/home/MemberController.java
@@ -1,5 +1,41 @@
 package com.kedu.home;
 
-public class MemberController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kedu.dto.MemberDTO;
+import com.kedu.services.MemberService;
+
+@Controller
+@RequestMapping("/Member")
+public class MemberController {
+	
+	@Autowired
+	private MemberService mServ;
+	
+	@RequestMapping("/add")
+	public String add(MemberDTO dto) {
+		mServ.insertMember(dto);
+		return "/admin/home";
+	}
+	@ResponseBody
+	@RequestMapping("/selectAll")
+	public List<MemberDTO> selectAll() {
+		return mServ.selectAll();
+	}
+	@RequestMapping("/update")
+	public String update(MemberDTO dto) {
+		mServ.update(dto);
+		return "/admin/home";
+	}
+	@RequestMapping("/delete")
+	public String delete(String id) {
+		mServ.delete(id);
+		return "/admin/home";
+	}
+	
 }
--- src/main/java/com/kedu/services/MemberService.java
+++ src/main/java/com/kedu/services/MemberService.java
@@ -1,5 +1,38 @@
 package com.kedu.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kedu.dao.MemberDAOImp;
+import com.kedu.dto.MemberDTO;
+
+@Service
 public class MemberService {
+
+	@Autowired
+	private MemberDAOImp daom;
+	
+	public void insertMember(MemberDTO dto) {
+		daom.insertMember(dto);
+	}
+
+	public List<MemberDTO> selectAll() {
+		return daom.selectAllMembers();
+		
+	}
+
+	public void update(MemberDTO dto) {
+		daom.updateMember(dto);
+		
+	}
+
+	public void delete(String id) {
+		daom.deleteMember(id);
+	}
+	
+	
+
 
 }
